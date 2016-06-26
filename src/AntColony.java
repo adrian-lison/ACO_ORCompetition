d9e904be68707354c420fc86dba84ef204fb6a95
Max_2 <Max_2@Max>
2016-06-26 22:39:58 +0200

    xy

--- src/AntColony.java
+++ src/AntColony.java
@@ -1,7 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class AntColony {
 
 	int numberOfAnts;
+	
+	int DIESEVARIABLELOESCHEN;
+	
 	int maxRounds;
 	ProblemInstance problem;
 	Solution globalBest;
@@ -12,12 +20,97 @@ public class AntColony {
 	}
 	
 	public static void main(String[] args){
+		constructProblem("xx.txt");
 		//TODO
 	}
 	
 	private void constructProblem(String fileName){
-		//TODO
+		
+		String splitElement="#";
+		try {
+
+			FileReader fr=new FileReader(fileName);
+			BufferedReader br = new BufferedReader(fr);
+			
+			
+			int status = 0; //represents the kind of data extracted in current line: -1 => Start; 0=>Meta ; 1=>Nodes 
+			
+			String line=br.readLine();
+			while (line != null) {
+
+				switch(line){
+					case "#META":
+status=0;						
+break;
+					case "#NODES":
+						status=1;						
+						break;
+						break;
+					case "#EOF":
+						return;
+						
+					default:
+							String[] splitted = line.split(" ");
+							
+							
+							if(status==0){
+								//metadata
+								if(splitted[0].equals("name")){
+									String name="";
+									for(int i=2;i<splitted.length;i++){
+										name += splitted[i];
+									}
+									System.out.println("Name: " + name);
+								}
+								else if(splitted[0].equals("k"))
+									System.out.println("k: " +splitted[2]);
+								else if(splitted[0].equals("N"))
+									System.out.println("N: " +splitted[2]);
+								}
+							else if (status ==1){
+								//nodes
+								
+								String coordinates = splitted[1];
+								coordinatesString.replace('(', ''); coordinatesString.replace(')', '');
+								
+								coordinates =split(",");
+								System.out.print("x:" + coordinates[0] + " y:" + coordinates[1]);
+								
+								
+								String customerDetailsString = splitted[2];
+								customerDetailsString.replace('(', ''); customerDetails.replace(')','');
+								
+								customerDetails = customerDetailsString.split(",");
+								System.out.print("weight:" + customerDetails[0] + " profit:" + customerDetails[1]);
+
+							}
+							
+							}
+				
+				line=br.readLine();
+			}
+
+			
+			//XXX Achtung Plagiat
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+		
 	}
+	
+	
 	
 	private Solution execute(){
 		//TODO
--- src/Logic.java
+++ src/Logic.java
@@ -1,4 +1,39 @@
 
 public class Logic {
 
+	private int pheromoneWeight;
+	private int heuristicWeight;
+	RandomNumberGenerator random;
+	AntColony colony;
+	
+	public Logic(){
+		random = new JavaRandom();
+	}
+	
+	public Customer decideProblem(ProblemInstance problem, Solution currentSolution ){
+		return new Customer(heuristicWeight, null, heuristicWeight, heuristicWeight);
+		
+		Customer pci = colony.getPCI(this); //YYY
+		
+		//pick customer
+		Customer c=new Customer(1,null,1,1); //XXX TO-DO
+		Customer c1=null; //XXX TO-DO
+		Customer c2=null; //XXX TO-DO
+		
+		
+		//calculate economic value
+		double customerValue = c.getUse()-c.getWeight();
+		if(customerValue < c1.distanceTo(c2))
+			System.out.println("Customer c2 kommt nicht infrage, weil zusaetzlicher value geringer ist als der zusaetzliche Weg(50/50 Gewichtung)XXX");
+		
+		double economicValue = (heuristicWeight*customerValue + problem.getPheromoneValue(c1,c2)*pheromoneWeight) / (heuristicWeight+pheromoneWeight);
+
+		//compare Values
+		
+		
+		
+		
+	}
+	
+	
 }
